ec5973b7cc16f7e8dbe874a6db94b89ba8c6e0fa
RyotaroOkabe <rytrokb@gmail.com>
2023-09-16 20:43:33 -0400

    phono3py

--- screen_mpid_tdep.py
+++ screen_mpid_tdep.py
@@ -8,15 +8,15 @@ ndim = 2
 njob = 1    # number of parallel jobs
 N=1
 n=32
-cluster='ornl'
-queue='batch'    #'skylake'
+# cluster='ornl'
+# queue='batch'    #'skylake'
 maxdisps = None	# 3    # stop running job if certain number of disp-*abo are completed.
-maxjobs = 5    # max number of jobs to submit at one time. 
+maxjobs = 9    # max number of jobs to submit at one time. 
 screen='_'
 other_screens = ['X']
-run_phohno3py=True
+run_phohno3py=False
 run_tdep_init=False
-jobdir_run = jobdir
+jobdir_run = jobdir2
 all_jobdirs = [jobdir_run]  #, jobdir2]
 temp = 300  # ttemperature for TDEP
 max_freq = 20   # max frequency for TDEP
@@ -44,13 +44,14 @@ print('mpid: ', len(mpids))
 
 #%%
 if run_phohno3py:
-    get_scripts(mpids,subid,nomaddir,jobdir_run,psdir,ndim,N,n,queue,screen=screen,njob=1,cluster=cluster,archive=True)
+    get_scripts(mpids,subid,nomaddir,jobdir_run,psdir,ndim,N,n,queue,screen=screen,njob=1,cluster=cluster,archive=run_tdep_init)
 
 #%%
+mpids = mpids[:20]
 if run_tdep_init:
     tdep_cells_all(mpids, r_ss, [ndim,ndim,ndim], temp, max_freq, jobdir_run, stdep=use_stdep)
 
 #%%
-# screen_mpids(mpids, maxdisps, maxjobs, skips, jobdir_run, logs_dir, screen, queue, other_screens, all_jobdirs)
+screen_mpids(mpids, maxdisps, maxjobs, skips, jobdir_run, logs_dir, screen, queue, other_screens, all_jobdirs)
 
 #%%
